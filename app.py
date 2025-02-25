96afebaeb27f3add9559e2b01ccb05f68aa36bc4
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2025-02-25 23:44:53 +0400

    Using the UUID and optimize the store

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
+import uuid
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name":"My Store",
-        "Items":[
-            {
-                "name":"Chair",
-                "price":15.99
-            }
-        ]
-    }
-]
+# stores = [
+#     {
+#         "name":"My Store",
+#         "Items":[
+#             {
+#                 "name":"Chair",
+#                 "price":15.99
+#             }
+#         ]
+#     }
+# ]
 @app.get("/store") # http://127.0.0.1:5000/store
 def get_stores():
-    return {"stores":stores}
+    return {"stores": list(stores.values())}
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name":request_data["name"],"items":[]}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id":store_id}
+    stores[store_id] = store
+    return store, 201
 
 @app.post("/store/<string:name>/item")
 def create_item(name):
@@ -34,12 +37,12 @@ def create_item(name):
             return new_item, 201
     return {"message": "Store not found"}, 404 
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message":"Store not found"}, 404
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message":"Store not found"}, 404
 
 
 @app.get("/store/<string:name>/item")
