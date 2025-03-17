52d4cc384e8be9767336644032372979f33f4480
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2025-03-17 10:08:15 +0400

    more update with smorest

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -22,17 +23,41 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
     store_id = uuid.uuid4().hex
     store = {**store_data, "id":store_id}
     stores[store_id] = store
     return store, 201
 
 @app.post("/item")
-def create_item(name):
+def create_item():
     item_data = request.get_json()
+    if(
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload",
+        )
+    for item in items.values():
+        if(
+            item_data["name"]== item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+            
     if item_data["store_id"] not in stores:
-        return {"message":"Store not found"}, 404
-    
+        abort(404, message="Store not found")
+            
     item_id = uuid.uuid4().hex
     item = {**item_data, "id":item_id}
     items[item_id] = item
@@ -49,11 +74,11 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message":"Store not found"}, 404
+        abort(404, message="Store not found")
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message":"Store not found"}, 404
+        abort(404, message="Store not found")
