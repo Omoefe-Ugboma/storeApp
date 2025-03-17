2f34fbe7eb59d9ccc5eacb386dbe462af863f28b
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2025-03-17 12:57:20 +0400

    delete function implemented

--- app.py
+++ app.py
@@ -55,14 +55,14 @@ def create_item():
         ):
             abort(400, message=f"Item already exists.")
             
-    if item_data["store_id"] not in stores:
-        abort(404, message="Store not found")
+    # if item_data["store_id"] not in stores:
+    #     abort(404, message="Store not found")
             
     item_id = uuid.uuid4().hex
     item = {**item_data, "id":item_id}
     items[item_id] = item
     
-    return item, 201
+    return item
 
 @app.get("/item")
 def get_all_items():
@@ -82,3 +82,11 @@ def get_item(item_id):
         return items[item_id]
     except KeyError:
         abort(404, message="Store not found")
+        
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message":"Item deleted"}
+    except KeyError:
+        abort(404, message="Item not found.")
